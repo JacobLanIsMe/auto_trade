a7b695c81710aaa6d4fe91b9929835eeb2775e2e
Jacob <maimaisatt@gmail.com>
2025-06-26 23:55:09 +0800

    Change the folder name "Service" to "service"

--- data_migration/data_migration.py
+++ data_migration/data_migration.py
@@ -1,8 +1,8 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from Service.sql_server_service import get_candidate_data as get_sql_data
-from Service.mongodb_service import get_candidate_data as get_mongo_data
+from service.sql_server_service import get_candidate_data as get_sql_data
+from service.mongodb_service import get_candidate_data as get_mongo_data
 
 def main():
     print('--- SQL Server Data ---')
